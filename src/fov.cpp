ddce63c5891859346cd3ab40d3a2877902fb0fdf
Clippy95 <qpzezoqp15@gmail.com>
2026-01-20 13:08:50 +0300

    cg_fov_fix_lowfovads and cg_fov_fix_lowfovads_default_fov

--- src/fov.cpp
+++ src/fov.cpp
@@ -18,6 +18,11 @@
 namespace fov {
 	uintptr_t CG_GetViewFov_ptr;
 	dvar_s* cg_fovscale;
+	dvar_s* cg_fov;
+	dvar_s* cg_fov_fix_lowfovads;
+
+	dvar_s* cg_fov_fix_lowfovads_default_fov;
+
 	double CG_GetViewFov_hook() {
 		auto fov = cdecl_call<double>(CG_GetViewFov_ptr);
 		if (cg_fovscale) {
@@ -26,19 +31,54 @@ namespace fov {
 		return fov;
 	}
 
+	void HandleWeaponADS_hack(float* current_weapon_fov) {
+		if (cg_fov) {
+			if (cg_fov_fix_lowfovads && cg_fov_fix_lowfovads->value.integer == 1) {
+				if (cg_fov->value.decimal < *current_weapon_fov) {
+					float value = cg_fov->value.decimal / cg_fov_fix_lowfovads_default_fov->value.decimal;
+					*current_weapon_fov *= value;
+				}
+			}
+			else if (cg_fov_fix_lowfovads && cg_fov_fix_lowfovads->value.integer >= 2) {
+				if (cg_fov->value.decimal < cg_fov_fix_lowfovads_default_fov->value.decimal) {
+					float value = cg_fov->value.decimal / cg_fov_fix_lowfovads_default_fov->value.decimal;
+					*current_weapon_fov *= value;
+				}
+			}
+		}
+
+		//if (cg_fovscale_ads) {
+		//	*current_weapon_fov *= std::clamp(cg_fovscale_ads->value, 0.1f, FLT_MAX);
+		//}
+
+	}
+
 	class component final : public component_interface
 	{
 	public:
 		void post_gfx() override {
 			if (!cg_fovscale) {
 				cg_fovscale = dvars::Dvar_RegisterFloat("cg_fovscale", 1.f, 0.01f, 2.f, DVAR_ARCHIVE);
-
+				cg_fov_fix_lowfovads = dvars::Dvar_RegisterInt("cg_fov_fix_lowfovads", 1, 0, 2, DVAR_ARCHIVE);
+				cg_fov_fix_lowfovads_default_fov = dvars::Dvar_RegisterFloat("cg_fov_fix_lowfovads_default_fov", 90.f, 1.f, 160.f, DVAR_ARCHIVE);
 				Memory::VP::InterceptCall(exe(0x4AE8E1), CG_GetViewFov_ptr,CG_GetViewFov_hook);
+				Memory::VP::Nop(exe(0x004AE7EF), 6);
+				static auto WeaponADS_hack = safetyhook::create_mid(exe(0x004AE7EF), [](SafetyHookContext& ctx) {
+					float current_weapon_fov_ads = *(float*)(ctx.ecx + 0x268);
+
+					HandleWeaponADS_hack(&current_weapon_fov_ads);
+					FPU::FLD(current_weapon_fov_ads);
+					});
 
 			}
 
 
 		}
+
+		void post_cg_init() override {
+			cg_fov = dvars::Dvar_FindVar("cg_fov");
+		}
+
 	};
 
 
